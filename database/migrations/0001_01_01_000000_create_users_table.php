afdfee3c9b89042668312c6eb4aeaf326ae8dad9
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-12-02 19:55:59 +0800

    password nulal

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,10 +16,11 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('provider')->default('CREDENTIALS'); // CREDENTIALS , GOOGLE , FACEBOOK , and every o auth :)
             $table->string('provider_id')->nullable(); //
             $table->string('social_avatar')->nullable();
+            
             $table->rememberToken();
             $table->timestamps();
         });
