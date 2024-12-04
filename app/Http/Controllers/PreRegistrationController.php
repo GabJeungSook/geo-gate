21c84d0be1114449435dc0856f90f9fbe8e4f927
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-12-04 19:24:48 +0800

    update add vladiatio

--- app/Http/Controllers/PreRegistrationController.php
+++ app/Http/Controllers/PreRegistrationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use is;
 use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
+use App\Models\EventSchedule;
 use App\Models\PreRegistration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -49,41 +51,47 @@ class PreRegistrationController extends Controller
     {
         //
     }
-
     public function createOrUpdatePreRegistration(Request $request)
-{
-    $validatedData = $request->validate([
-        'event_schedule_id' => 'required|exists:event_schedules,id', // Validate the event schedule
-        'qr_code' => 'nullable|string',
-    ]);
+    {
+        $validatedData = $request->validate([
+            'event_schedule_id' => 'required|exists:event_schedules,id',
+            'qr_code' => 'nullable|string',
+        ]);
+    
 
-   
-    $existingPreRegistration = PreRegistration::where('event_schedule_id', $validatedData['event_schedule_id'])
-                                                ->where('user_id', $request->user()->id)
-                                                ->first();
-
-    DB::beginTransaction();
-
-    try {
-        if ($existingPreRegistration) {
-           
-            $existingPreRegistration->update($validatedData);
-            $message = 'Pre-registration updated successfully';
-        } else {
-            // Create a new pre-registration
-            $validatedData['user_id'] = $request->user()->id;
-            PreRegistration::create($validatedData);
-            $message = 'Pre-registration created successfully';
+        $eventSchedule = EventSchedule::findOrFail($validatedData['event_schedule_id']);
+        if (!$eventSchedule->is_active) {
+            return ApiResponse::error('The schedule is no longer active.', 400); 
         }
-
-        DB::commit();
-
-        return ApiResponse::success([], $message);
-    } catch (\Exception $e) {
-        DB::rollBack();
-
-        return ApiResponse::error('An error occurred while processing your pre-registration', 500);
+    
+    
+        $existingPreRegistration = PreRegistration::where('event_schedule_id', $validatedData['event_schedule_id'])
+            ->where('user_id', $request->user()->id)
+            ->first();
+    
+        DB::beginTransaction();
+    
+        try {
+            if ($existingPreRegistration) {
+             
+                $existingPreRegistration->update($validatedData);
+                $message = 'Pre-registration updated successfully';
+            } else {
+            
+                $validatedData['user_id'] = $request->user()->id;
+                PreRegistration::create($validatedData);
+                $message = 'Pre-registration created successfully';
+            }
+    
+            DB::commit();
+    
+            return ApiResponse::success([], $message);
+        } catch (\Exception $e) {
+            DB::rollBack();
+    
+            return ApiResponse::error('An error occurred while processing your pre-registration'.$e->getMessage()   , 500);
+        }
     }
-}
+    
 
 }
