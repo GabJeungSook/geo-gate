ccc3651eaf2fc0f74feb2203ef99e609a8a90195
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-12-04 10:40:25 +0800

    uopdate full name

--- app/Http/Resources/UserDetailsResource.php
+++ app/Http/Resources/UserDetailsResource.php
@@ -21,8 +21,10 @@ class UserDetailsResource extends JsonResource
             'first_name' => $this->first_name ?? null,
             'last_name' => $this->last_name ?? null,
             'full_address' => $this->full_address ?? null,
+            'fullname' => $this->full_name, 
             'birthday' => $this->birthday ? Carbon::parse($this->birthday)->format('F d, Y') : null,
-            'course_id' => $this->course_id ?? null,
+            'course' => $this->course_id ?? null,
+
            
         ];
 }
